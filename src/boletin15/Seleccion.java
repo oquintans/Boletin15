b5bc5fc70293a7fa10bc8871d5ec6ac880afbef8
oquintansocampo <oquintansocampo@Eniac28.danielcastelao.org>
2015-01-12 09:31:04 +0100

    javadoc

--- src/boletin15/Seleccion.java
+++ src/boletin15/Seleccion.java
@@ -12,10 +12,20 @@ public class Seleccion {
     private int id;
     private int edad;
 
+    /**
+     *
+     */
     public Seleccion() {
 
     }
 
+    /**
+     *
+     * @param n
+     * @param a
+     * @param id
+     * @param e
+     */
     public Seleccion(String n, String a, int id, int e) {
 
         nombre = n;
@@ -40,38 +50,72 @@ public class Seleccion {
         this.nombre = nombre;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getApellidos() {
         return apellidos;
     }
 
+    /**
+     *
+     * @param apellidos
+     */
     public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     *
+     * @param id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getEdad() {
         return edad;
     }
 
+    /**
+     *
+     * @param edad
+     */
     public void setEdad(int edad) {
         this.edad = edad;
     }
 
+    /**
+     *
+     */
     public void concentrarse() {
         System.out.println("concentrase a seleccion");
     }
 
+    /**
+     *
+     */
     public void viajar() {
         System.out.println("viaxa a seleccion");
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     public String toString() {
         return super.toString() + "Seleccion{" + "nombre=" + nombre + ", apellidos=" + apellidos + ", id=" + id + ", edad=" + edad + '}';
